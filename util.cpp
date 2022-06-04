f52a3d8f100cbd5e70091804655fcb0074ddb0b7
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-06-04 00:43:01 -0400

    By using XoshiroCpp the random functions are now much faster.

--- util.cpp
+++ util.cpp
@@ -2,32 +2,41 @@
 #include <iomanip>
 #include <sstream>
 #include <string>
+#include <chrono>
 #include "util.h"
+#include "XoshiroCpp.hpp"
 
 using namespace std;
 
+#define RNG_SEED std::chrono::system_clock::now().time_since_epoch().count()
+
 int randInt( int min, int max)
  {
-    std::random_device                  rand_dev;
-    std::mt19937                        gen(rand_dev());
-    std::uniform_int_distribution<>    distr(min, max-1);
-    return distr(gen);
+    // std::random_device                  rand_dev;
+    // std::mt19937                        gen(rand_dev());
+    // std::uniform_int_distribution<>    distr(min, max-1);
+    
+    XoshiroCpp::Xoshiro256StarStar rng(RNG_SEED);
+
+    std::uniform_int_distribution<int> distr(min, max-1);
+    return distr(rng);
 }
 
 float randFloat( float min, float max)
  {
-    std::random_device                  rand_dev;
-    std::mt19937                        gen(rand_dev());
-    std::uniform_real_distribution<>    distr(min, max-1);
-    return distr(gen);
+    XoshiroCpp::Xoshiro256StarStar rng(RNG_SEED);
+
+    std::uniform_real_distribution<float> distr(min, max-1);
+    return distr(rng);
 }
+
 
 float random()
  {
-    std::random_device                  rand_dev;
-    std::mt19937                        gen(rand_dev());
-    std::uniform_real_distribution<>    distr(0,1);
-    return distr(gen);
+    XoshiroCpp::Xoshiro256StarStar rng(RNG_SEED);
+
+    std::uniform_real_distribution<float> distr(0,1);
+    return distr(rng);
 }
 
 string intToHex(int toHex,int size) {
