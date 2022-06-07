6cd94f0e33112dd98d143fc8cad6f6c2ce8a313b
binaryclock03 <binaryclock03@gmail.com>
2022-06-07 15:53:04 -0400

    directions now fully supported

--- netObjects/NeuralNet.cpp
+++ netObjects/NeuralNet.cpp
@@ -116,7 +116,37 @@ void NeuralNet::setDireciton(int dir)
 
 void NeuralNet::setDireciton(int x, int y)
 {
-    //does not work
+    if (x == 0 && y == 0)
+        this->direction = 0;
+
+    else if (x == 0 && y == 1)
+        this->direction = 1;
+
+    else if (x == 1 && y == 1)
+        this->direction = 2;
+
+    else if (x == 1 && y == 0)
+        this->direction = 3;
+
+    else if (x == 1 && y == -1)
+        this->direction = 4;
+
+    else if (x == 0 && y == -1)
+        this->direction = 5;
+
+    else if (x == -1 && y == -1)
+        this->direction = 6;
+
+    else if (x == -1 && y == 0)
+        this->direction = 7;
+
+    else if (x == -1 && y == 1)
+        this->direction = 8;
+}
+
+int NeuralNet::getDirection()
+{
+    return this->direction;
 }
 
 int NeuralNet::getId()
